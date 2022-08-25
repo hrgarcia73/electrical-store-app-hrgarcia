55063a1e54b137e8ff27a4b1c4bb3db57ad00d40
Ricardo Garcia <hrgarcia73@yahoo.com.ar>
2022-08-25 18:15:21 -0300

    Desafío Contador con botón

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,12 +1,17 @@
 
 import React, { useEffect, useState } from "react";
 import './ItemListContainer.css';
+import ItemCount from '../ItemCount/ItemCount';
 
 const ItemListContainer = ({mensaje}) =>{
 
   return (
     <div className='list'>
       <p>{mensaje}</p>
+      <ItemCount 
+        stock={10} 
+        initial={1} 
+        onAdd={(n)=> alert(`Agregados ${n} productos`)}/>
     </div>
   )
 }
