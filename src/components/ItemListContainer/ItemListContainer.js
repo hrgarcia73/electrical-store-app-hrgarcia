27abf93dfbd47921cf1e0726feb316adb84dff6f
Ricardo Garcia <hrgarcia73@yahoo.com.ar>
2022-08-25 13:11:54 -0300

    Agregado de prpiedad y mensaje en ItemListContainer

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -2,11 +2,11 @@
 import React, { useEffect, useState } from "react";
 import './ItemListContainer.css';
 
-const ItemListContainer = () =>{
+const ItemListContainer = ({mensaje}) =>{
 
   return (
     <div className='list'>
-      <p>Este es mi ItemListContainer</p>
+      <p>{mensaje}</p>
     </div>
   )
 }
